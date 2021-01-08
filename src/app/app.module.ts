bbb135b68df626a5fdf1fcc0b9ff72b6738292b7
MathisBurger <handymathis@gmail.com>
2021-01-08 23:10:30 +0100

    Added pre-edited design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ChangelogComponent } from './changelog/changelog.component';
+import { FAQComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'pricing', component: PricingComponent},
+  {path: 'changelog', component: ChangelogComponent},
+  {path: 'faq', component: FAQComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    PricingComponent,
+    ChangelogComponent,
+    FAQComponent,
+    ContactComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
